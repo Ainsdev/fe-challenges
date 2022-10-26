07c099c199641d84765dcde701f7d6584fd7e770
Ainsdev <ainsn1654@gmail.com>
2022-10-26 19:37:34 -0300

    finished the challenge

--- pages/ecommerce.tsx
+++ pages/ecommerce.tsx
@@ -10,7 +10,11 @@ import { Product } from '../interfaces'
 
 const WeatherPage: NextPage<{ data: Product[] }> = (props) => {
     const [drawer, setDrawer] = useState(false);
-    const [value, setValue] = useState(1)
+    const [value, setValue] = useState(1);
+    const [priceRange, setpriceRange] = useState({
+        low: 0,
+        high: 1000,
+    });
     const options = [
         { label: 'London', value: 'london' },
         { label: 'Sydney', value: 'sydney' },
@@ -22,20 +26,31 @@ const WeatherPage: NextPage<{ data: Product[] }> = (props) => {
         { label: 'Shanghai', value: 'shanghai' },
         { label: 'Shanghai', value: 'shanghai' },
     ]
-    //data
-    
-    const [data, setData] = useState(props.data)
-    
+    //managing data
+    const [data, setData] = useState(props.data.slice(0, 12))
+    const loadMoreProducts = (e: { preventDefault: () => void }) => {
+        e.preventDefault();
+        setData(props.data.slice(0, data.length + 12))
+    }
+    //manage data with filters
+    const searchByCategories = (e: { preventDefault: () => void },n:number) => {
+        e.preventDefault();
+        setData(props.data.filter((item) => item.categoryId == n).slice(0, 12))
+    }
+    const searchByPrice = (e: { preventDefault: () => void }) => {
+        e.preventDefault();
+        setData(props.data.filter((item) => item.price >= priceRange.low && item.price <= priceRange.high).slice(0, 12))
+    }
     return (
         <Layout title='Ecommerce' link='https://drive.google.com/file/d/1GOIGYGYcExKks6qGrKTTnHEz7nQmMeTp/view?usp=sharing'>
             <main className={styles.main}>
                 <h1>The Best Ecommerce</h1>
                 <nav>
-                    <Button auto width="30%" mx="5px">Books</Button>
-                    <Button auto width="30%" mx="5px">Home</Button>
-                    <Button auto width="30%" mx="5px">Kids</Button>
-                    <Button auto width="30%" mx="5px">Health</Button>
-                    <Button auto width="30%" mx="5px">Clothing</Button>
+                    <Button onClick={(e)=>searchByCategories(e,1)} auto width="30%" mx="5px">Books</Button>
+                    <Button onClick={(e) => searchByCategories(e, 2)} auto width="30%" mx="5px">Home</Button>
+                    <Button onClick={(e)=>searchByCategories(e,3)} auto width="30%" mx="5px">Kids</Button>
+                    <Button onClick={(e)=>searchByCategories(e,4)} auto width="30%" mx="5px">Health</Button>
+                    <Button onClick={(e)=>searchByCategories(e,5)} auto width="30%" mx="5px">Clothing</Button>
                 </nav>
                 <section className={styles.products}>
                     <Button type='secondary' auto onClick={() => setDrawer(true)} >Show Filters</Button>
@@ -45,9 +60,10 @@ const WeatherPage: NextPage<{ data: Product[] }> = (props) => {
                             <Grid.Container gap={2} justify="center">
                                 {data.map((product) => (
                                     <Grid xs={24} sm={12} md={8} lg={6} key={product.id}>
-                                        <ProductCard name={product.name} price={product.price} rating={product.rating} image={'http://placeimg.com/640/480/'+product.id.toString()} />
+                                        <ProductCard name={product.name} price={product.price} rating={product.rating} image={'http://placeimg.com/640/480/' + product.id.toString()} />
                                     </Grid>
                                 ))}
+                                <Button onClick={(e) => loadMoreProducts(e)}>Load More</Button>
                             </Grid.Container>
                         </div>
                     </section>
@@ -64,13 +80,19 @@ const WeatherPage: NextPage<{ data: Product[] }> = (props) => {
                                     <p>By Pricing</p>
                                     <>
                                         <p>From: ($)</p>
-                                        <Slider step={10} max={1000} min={0} initialValue={0} showMarkers width="75%" />
+                                        <Slider onChange={(val)=>setpriceRange({
+                                            low: val,
+                                            high: priceRange.high
+                                        })} value={priceRange.low} step={10} max={1000} min={0} initialValue={0} showMarkers width="75%" />
                                         <Spacer />
                                         <p>To: ($)</p>
-                                        <Slider step={10} max={1000} min={10} initialValue={10} showMarkers width="75%" />
+                                        <Slider onChange={(val)=>setpriceRange({
+                                            low: priceRange.low,
+                                            high: val
+                                        })} value={priceRange.high}  step={10} max={1000} min={10} showMarkers width="75%" />
                                     </>
                                     <Spacer />
-                                    <Button>Apply</Button>
+                                    <Button onClick={(e)=>searchByPrice(e)}>Apply</Button>
                                 </article>
                                 <article>
                                     <p>By Colors</p>
